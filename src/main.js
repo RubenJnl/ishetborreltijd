395ef72681ce2f6d895a7d24d085fb553f3cdeb1
RubenJnl <ruben@ubenjanssen.nl>
2019-12-16 19:58:11 +0100

    👀 Analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+import VueGtag from 'vue-gtag'
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: "UA-16618146-4" }
+});
 
 new Vue({
   router,
