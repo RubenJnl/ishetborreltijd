c48d28f5a13dc2edb0869c8e772617fb0a3fbe50
RubenJnl <hallo@rubenjanssen.nl>
2020-04-10 15:30:12 +0200

    🏗 Quick 'n dirty override added

--- src/router/index.js
+++ src/router/index.js
@@ -18,9 +18,18 @@ const routes = [
   //   // which is lazy-loaded when the route is visited.
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   // },
+  {
+    path: '/ja',
+    name: 'home',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Yes.vue'),
+  },
 ];
 
 const router = new VueRouter({
+  mode: `history`,
   routes,
 });
 
---
+++ src/views/Yes.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="home">
+    <Title title="Is het borreltijd?" :paragraph="paragraph"></Title>
+    <div class="pb">
+      <img src="@/assets/PoweredBy.gif" alt="PoweredBy Giphy" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Title from '@/components/Title.vue';
+import axios from 'axios'
+
+export default {
+  name: 'home',
+  components: {
+    Title,
+  },
+  data() {
+    return {
+      paragraph: this.updateParagraph(),
+      cheers: this.updateGif()
+    }
+  },
+  methods: {
+    updateParagraph() {
+      const ja = 'Ja, proost!',
+        nee = 'Nee, helaas nog geen borreltijd',
+        weekend = 'Nee, maar het is weekend 🎉',
+        date = new Date();
+      const hour = date.getHours(),
+        minute = date.getMinutes(),
+        day = date.getDay();
+      const paragraph = hour == 15 && minute >= 30 || hour >=16 ? ja : day === 6 || day === 0 ? weekend : ja;
+
+      return paragraph
+    },
+    updateGif() {
+      const date = new Date();
+      const hour = date.getHours(),
+        minute = date.getMinutes(),
+        day = date.getDay();
+
+			// const noWork = (hour == 15 && minute >= 30 || hour >=16) || hour <= 2 && (day === 0 || day === 5 || day === 6) ? true : false;
+			const noWork = true
+      const weekend = day === 6 || day === 0
+      let tag = 'working';
+      let context = this;
+      if (noWork){
+        tag = 'cheers';
+      } else if (weekend) {
+        tag = 'weekend';
+      }
+      const fetchObj = {
+        type: 'GET',
+        url: 'https://api.giphy.com/v1/gifs/search?api_key=pBQwNMPM0LVWZb5ACpUZPthByUY5KQ6o&rating=PG' + '&q=' + tag + '&limit=25',
+      };
+      axios(fetchObj)
+        .then(function (resp) {
+          // handle success
+          const obj = resp.data;
+          let rand = Math.floor(Math.random() * obj.data.length);
+          if (day !== 5 && (obj.data[rand].title.toLowerCase().indexOf('friday') >= 0 || obj.data[rand].slug.indexOf('friday') >= 0)){
+            rand++;
+          }
+          if (obj.data[rand] && obj.data[rand].type === 'gif'){
+            context.cheers = '"' + obj.data[rand].images.original.url + '"'
+          }
+        })
+        .catch((error) => {
+          /* eslint-disable no-console */
+          console.warning('auctionDetails ERROR', error);
+          if (noWork) {
+            // return "https://media0.giphy.com/media/8yFGAh0Zzj71K/giphy.gif"
+            return "https://media3.giphy.com/media/mFSEfZlTrgX3CD150c/giphy.gif"
+          } else {
+            // return "https://media2.giphy.com/media/lktRSbK7mCbXAFIxEc/giphy.gif"
+            return "https://media1.giphy.com/media/o0vwzuFwCGAFO/giphy.gif"
+          }
+        });
+    }
+  },
+  watch: {
+    cheers: function (cheers) {
+      document.documentElement.style.setProperty('--backgroundGif', `url(${cheers})`);
+    }
+  },
+  created() {
+    document.documentElement.style.setProperty('--backgroundGif', `url(${this.cheers})`);
+  }
+};
+</script>
